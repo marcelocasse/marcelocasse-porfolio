df11f2b5a82896f1b306b9958dfc93f0f2cbf50e
Marcelo Casse <marcelogabriel037@gmail.com>
2024-02-02 18:31:41 -0300

    update card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,3 +1,5 @@
+import { FaLink } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa6";
 const Card = ({title,desciption,image,tech,website,repository}) => {
     return (
         <div class="max-w-sm  border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
@@ -5,19 +7,18 @@ const Card = ({title,desciption,image,tech,website,repository}) => {
             <div class="p-5">
                 <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
                 <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{desciption}</p>
-                
-                
-                <a href={website} target="_blank" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    Ir al sitio web
-                    <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                    </svg>
+                <ul className=" flex text-center justify-center w-full gap-2 mb-3">
+                {
+                    tech.map((value) => (
+                        <li>{value}</li>
+                    ))
+                }
+                </ul>
+                <a href={website} target="_blank" class="inline-flex items-center border-2  gap-2 px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    <FaLink className="size-5"/> Preview
                 </a>
-                <a href={repository} target="_blank" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    Ver Código Fuente
-                    <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                    </svg>
+                <a href={repository} target="_blank" class="inline-flex items-center border-2 gap-2 px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                <FaGithub className="size-5"/> Code
                 </a>
             </div>
         </div>
